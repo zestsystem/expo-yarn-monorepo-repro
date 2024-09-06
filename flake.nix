da29df87b2ac517a6714cf1f41c98bb631d4a097
zestsystem <mikeyim@unitetheculture.org>
2024-09-06 13:29:41 -0700

    feat: setup nix flake

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Expo Yarn Workspace Monorepo";
+  inputs = {
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      formatter = pkgs.alejandra;
+
+      devShells = {
+        default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            # Node
+            nodejs_20
+            yarn-berry
+          ];
+        };
+      };
+    });
+}
